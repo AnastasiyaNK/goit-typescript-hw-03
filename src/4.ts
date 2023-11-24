fee2663d6948f9411efc4438852d758d53745f6a
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2023-11-24 11:08:19 +0100

    add 3 hw

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,54 @@
-const key = new Key();
+interface IKey {
+  getSignature: () => number;
+}
+interface IPerson {
+  getKey: () => IKey;
+}
+
+class Key implements IKey {
+  constructor(private signature: number) {
+    this.signature = signature;
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person implements IPerson {
+  constructor(private key: IKey) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  key: IKey | null = null;
+  tenants: IPerson[] = [];
+
+  comeIn(person: IPerson) {
+    if (this.door === true) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: IKey): void;
+}
+
+class MyHouse extends House {
+  constructor(key: IKey) {
+    super();
+    this.key = key;
+  }
+  openDoor(key: IKey) {
+    if (this.key?.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +56,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
